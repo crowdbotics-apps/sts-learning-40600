5c401694bc7d0410ee057a4536786ea4f4d66b5f
Crowdbotics <team@crowdbotics.com>
2023-05-10 05:55:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sts-learning-40600",
+  url: "https://sts-learning-40600.botics.co",
+  api: "https://sts-learning-40600.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
